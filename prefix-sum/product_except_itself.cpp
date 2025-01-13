838758cab8f4ad4d0c6b534a7e0b904e758926f2
khushboodixit03 <khushboodixit4687@gmail.com>
2025-01-13 14:42:50 +0530

    product

--- prefix-sum/product_except_itself.cpp
+++ prefix-sum/product_except_itself.cpp
@@ -1,15 +1,23 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-vector<int> suffixsum(vector<int>& v){
+vector<int> parth(vector<int>& v){
   int n = v.size();
-  vector<int> suff(n);
-  suff[n-1] = v[n-1];
-  for(int i = n-2; i >= 0; i--){
-    suff[i] = v[i] * suff[i+1];
+  vector<int> pre(n);
+  pre[0] = 1;
+  for(int i = 1; i < n; i++){
+    pre[i] = pre[i-1] * v[i-1]; 
   }
-  return suff;
-  
+  vector<int> suff(n);
+  suff[n-1] = 1;
+  for(int i = n-2; i >= 0; i--){
+    suff[i] = suff[i+1] * v[i+1];
+  }
+  vector<int> ans(n);
+  for(int i = 0; i < n; i++){
+    ans[i] = pre[i] * suff[i];
+  }
+  return ans;
 }
 int main(){
   int n;
@@ -21,7 +29,7 @@ int main(){
     cin>>x;
     v.push_back(x);
   }
-vector<int> ans = suffixsum(v);
+  vector<int> ans = parth(v);
   for(int i = 0; i < n; i++){
     cout<<ans[i]<<" ";
   }
