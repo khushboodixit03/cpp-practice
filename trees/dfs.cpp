a799c96fb7e57bc9ba02f2ee3d3785c99b42aef2
khushboodixit03 <khushboodixit4687@gmail.com>
2025-01-29 10:29:59 +0530

    dfs

---
+++ trees/dfs.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int val;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+
+    }
+};
+
+void preorder(Node* root){
+    if(root == NULL) return;
+    cout<<root->val<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void inorder(Node* root){
+    if(root == NULL) return;
+    inorder(root->left);
+    cout<<root->val<<" ";
+    inorder(root->right);
+}
+
+void postorder(Node* root){
+    if(root == NULL) return;
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->val<<" ";
+}
+
+
+int main(){
+    Node* a = new Node(23); 
+    Node* b = new Node(24); 
+    Node* c = new Node(21); 
+    Node* d = new Node(26); 
+    Node* e = new Node(29); 
+    Node* f = new Node(28); 
+    Node* g = new Node(30); 
+
+    a->left = b;
+    a->right = c;
+    b->left = d;
+    b->right = e;
+    c->left = f;
+    c->right = g;
+    
+    preorder(a);
+    cout<<endl;
+    inorder(a);
+    cout<<endl;
+    postorder(a);
+    cout<<endl;
+}
