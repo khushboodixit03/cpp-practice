1ed7e1de65760479bdb25dca17f8f0a21ee8a7c9
khushboodixit03 <khushboodixit4687@gmail.com>
2025-02-04 14:53:59 +0530

    sql1

---
+++ oops programming/sql_day_1.cpp
@@ -0,0 +1,11 @@
+create table employee(
+emp_name varchar(100),
+emp_id int,
+emp_salary int
+);
+ insert into employee values("khushboo",4,80000);
+ insert into employee values("parth",5,100000);
+ insert into employee values("naman",10,50000);
+ insert into employee values("billu",12,10000);
+ 
+ select * from employee;
